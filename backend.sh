c1ffcc9ca16b0a39720bc5d08d8bb369f7b7e058
n <norman.koch@tu-dresden.de>
2025-07-15 19:47:01 +0200

    auto install venv in slurm job

--- backend.sh
+++ backend.sh
@@ -8,15 +8,25 @@
 #SBATCH --time=8:00:00
 #SBATCH --output=logs/fastapi_%j.log
 
+SCRIPT_DIR=$(dirname $(realpath "$0"))
 
-# Load modules and activate environment
-module load release/24.04 GCC/12.3.0 OpenMPI/4.1.5
-module load PyTorch/2.1.2
+if [[ ! -z $SLURM_JOB_ID ]]; then
+	SCRIPT_DIR=$(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}')
 
-source /home/inbe405h/env/bin/activate
+	module load release/24.04 GCC/12.3.0 OpenMPI/4.1.5 PyTorch/2.1.2
+
+	VENV_DIR=$HOME/.squai_env
+	VENV_ACTIVATE=$VENV_DIR/bin/activate
+fi
+
+if [[ ! -d $VENV_DIR ]] || [[ ! -e $VENV_ACTIVATE ]]; then
+	python3 -mvenv $VENV_DIR
+fi
+
+source $VENV_ACTIVATE
 
 # Create log directory if it doesn't exist
-mkdir -p logs
+mkdir -p $SCRIPT_DIR/logs
 
 # Launch FastAPI with uvicorn
 uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1
