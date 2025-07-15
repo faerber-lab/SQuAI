21975bd1827f134f294389bc2f8f9cbb412f0020
n <norman.koch@tu-dresden.de>
2025-07-15 19:48:25 +0200

    install venv automatically

--- backend.sh
+++ backend.sh
@@ -9,18 +9,20 @@
 #SBATCH --output=logs/fastapi_%j.log
 
 VENV_DIR=$HOME/.squai_env
+VENV_ACTIVATE=$VENV_DIR/bin/activate
+
 SCRIPT_DIR=$(dirname $(realpath "$0"))
 
 if [[ ! -z $SLURM_JOB_ID ]]; then
 	SCRIPT_DIR=$(scontrol show job "$SLURM_JOB_ID" | awk -F= '/Command=/{print $2}')
 
 	module load release/24.04 GCC/12.3.0 OpenMPI/4.1.5 PyTorch/2.1.2
-
-	VENV_ACTIVATE=$VENV_DIR/bin/activate
 fi
 
 if [[ ! -d $VENV_DIR ]] || [[ ! -e $VENV_ACTIVATE ]]; then
-	python3 -mvenv $VENV_DIR
+	python3 -mvenv $VENV_ACTIVATE
+
+	pip install -r $SCRIPT_DIR/requirements.txt
 fi
 
 source $VENV_ACTIVATE
