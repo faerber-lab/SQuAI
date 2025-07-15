e09cee2db0e43a631e571b921b23710831f72606
n <norman.koch@tu-dresden.de>
2025-07-15 19:52:49 +0200

    added frontend.sh

---
+++ frontend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+VENV_DIR=$HOME/.squai_env_frontend
+VENV_ACTIVATE=$VENV_DIR/bin/activate
+
+SCRIPT_DIR=$(dirname $(realpath "$0"))
+
+if [[ ! -d $VENV_DIR ]] || [[ ! -e $VENV_ACTIVATE ]]; then
+	python3 -mvenv $VENV_DIR
+
+	source $VENV_ACTIVATE
+
+	pip install streamlit
+fi
+
+source $VENV_ACTIVATE
+
+streamlit app.py --server.port 8501 --server.address 0.0.0.0
